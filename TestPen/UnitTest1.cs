86f2788de32bc4935805eed4d0df8e75458f3be8
Akulenko <atanzilya@yahoo.co.uk>
2016-06-08 22:23:46 +0300

    add Unit Test project

---
+++ TestPen/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitProject;
+using System.IO;
+
+namespace TestPen
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Returns_Empty_String_If_Ink_Finished()
+        {
+            //arrange
+            var pen = new Pen(0);
+
+            //action
+            var result = pen.write("word");
+
+            //assert
+            Assert.AreEqual(result, "");
+        }
+
+        [TestMethod]
+        public void Write_Word_If_Pen_Works()
+        {
+            //arrange
+            var pen = new Pen(10);
+
+            //action
+            var result = pen.write("word");
+
+            //assert
+            Assert.AreEqual(result, "word");
+        }
+
+        [TestMethod]
+        public void Write_Part_Of_Word_While_Ink_Not_Finished_If_Pen_Works()
+        {
+            //arrange
+            var pen = new Pen(5, 2);
+
+            //action
+            var result = pen.write("worldddd");
+
+            //assert
+            Assert.AreEqual(result, "world");
+        }
+
+        [TestMethod]
+        public void Check_Correct_Colour_Of_Pen()
+        {
+            //arrange
+            var pen = new Pen(5, 1.0, "RED");
+
+            //action
+            var result = pen.getColor();
+
+            //assert
+            Assert.AreEqual(result, "RED");
+        }
+
+        [TestMethod]
+        public void Writes_Pen_Color_To_The_Console()
+        {
+            //arrange
+            var pen = new Pen(5, 1, "RED");
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            //action
+            pen.doSomethingElse();
+
+            //assert
+            string expected = string.Format("RED{0}", Environment.NewLine);
+            Assert.AreEqual(expected, sw.ToString());
+
+            sw.Close();
+            sw.Dispose();
+        }
+    }
+}
---
+++ UnitProject/Pen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitProject
+{
+    public class Pen
+    {
+        // сколько чернила в ручке
+        private int inkContainerValue = 1000;
+        // размер букв, которые пишутся ручкой
+        private double sizeLetter = 1.0;
+        // цвет ручки
+        private String color = "BLUE";
+
+        public Pen(int inkContainerValue)
+        {
+            this.inkContainerValue = inkContainerValue;
+        }
+
+        public Pen(int inkContainerValue, double sizeLetter): this(inkContainerValue)
+        {
+            this.sizeLetter = sizeLetter;
+        }
+
+        public Pen(int inkContainerValue, double sizeLetter, String color): this(inkContainerValue, sizeLetter)
+        {
+            this.color = color;
+        }
+
+        public String write(String word)
+        {
+            if (!isWork())
+            {
+                return "";
+            }
+            double sizeOfWord = word.Length * sizeLetter;
+            if (sizeOfWord <= inkContainerValue)
+            {
+                inkContainerValue -= (int)sizeOfWord;
+                return word;
+            }
+            String partOfWord = word.Substring(0, inkContainerValue);
+            inkContainerValue = 0;
+            return partOfWord;
+        }
+
+        // ERROR!!! Bug
+        public String getColor()
+        {
+            return "BLUE";
+        }
+
+        public Boolean isWork()
+        {
+            return inkContainerValue > 0;
+        }
+        public void doSomethingElse()
+        {
+            Console.WriteLine(color);
+        }
+    }
+}
